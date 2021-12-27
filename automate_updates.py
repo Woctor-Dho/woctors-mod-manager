224c3a9ae00b765757a5a6bb0350e4b202280e32
Woctor-Dho <Woctor-Dho@users.noreply.github.com>
2021-12-26 22:07:32 -0600

    fix a few bugs, finish adding mods

--- automate_updates.py
+++ automate_updates.py
@@ -28,12 +28,156 @@ class Source(enum.Enum):
     CURSEFORGE = 1
     MODRINTH = 2
 
+SOURCE_TEMPLATE = [
+    {
+        "name": "fabric_api",
+        "source": Source.CURSEFORGE,
+        "mod_id": 306612,
+        "output_dir": "mods",
+    },
+    {
+        "name": "cloth_config",
+        "source": Source.CURSEFORGE,
+        "mod_id": 319057,
+        "output_dir": "mods",
+    },
+    {
+        "name": "ding",
+        "source": Source.CURSEFORGE,
+        "mod_id": 231275,
+        "output_dir": "mods",
+    },
+    {
+        "name": "bears_armour_hud",
+        "source": Source.CURSEFORGE,
+        "mod_id": 420155,
+        "output_dir": "mods",
+    },
+    {
+        "name": "better_taskbar",
+        "source": Source.MODRINTH,
+        "mod_id": "gPEcet33",
+        "output_dir": "mods",
+    },
+    {
+        "name": "dark_loading_screen",
+        "source": Source.CURSEFORGE,
+        "mod_id": 365727,
+        "output_dir": "mods",
+    },
+    {
+        "name": "falling_leaves",
+        "source": Source.MODRINTH,
+        "mod_id": "WhbRG4iK",
+        "output_dir": "mods",
+    },
+    {
+        "name": "ferrite_core",
+        "source": Source.MODRINTH,
+        "mod_id": "uXXizFIs",
+        "output_dir": "mods",
+    },
+    {
+        "name": "Iris",
+        "source": Source.MODRINTH,
+        "mod_id": "YL57xq9U",
+        "output_dir": "mods",
+    },
+    {
+        "name": "item_scroller",
+        "source": Source.CURSEFORGE,
+        "mod_id": 242064,
+        "output_dir": "mods",
+    },
+    {
+        "name": "krypton",
+        "source": Source.MODRINTH,
+        "mod_id": "fQEb0iXm",
+        "output_dir": "mods",
+    },
+    {
+        "name": "lazy_dfu",
+        "source": Source.MODRINTH,
+        "mod_id": "hvFnDODi",
+        "output_dir": "mods",
+    },
+    {
+        "name": "lazy_language_loader",
+        "source": Source.MODRINTH,
+        "mod_id": "Nz0RSWrF",
+        "output_dir": "mods",
+    },
+    {
+        "name": "malilib",
+        "source": Source.CURSEFORGE,
+        "mod_id": 303119,
+        "output_dir": "mods",
+    },
+    {
+        "name": "mod_menu",
+        "source": Source.MODRINTH,
+        "mod_id": "mOgUt4GM",
+        "output_dir": "mods",
+    },
+    {
+        "name": "ok_zoomer",
+        "source": Source.MODRINTH,
+        "mod_id": "aXf2OSFU",
+        "output_dir": "mods",
+    },
+    {
+        "name": "rebind_narrator",
+        "source": Source.MODRINTH,
+        "mod_id": "qw2Ls89j",
+        "output_dir": "mods",
+    },
+    {
+        "name": "sodium",
+        "source": Source.MODRINTH,
+        "mod_id": "AANobbMI",
+        "output_dir": "mods",
+    },
+    {
+        "name": "sodium-extra",
+        "source": Source.MODRINTH,
+        "mod_id": "PtjYWJkn",
+        "output_dir": "mods",
+    },
+    {
+        "name": "starlight_fabric",
+        "source": Source.MODRINTH,
+        "mod_id": "H8CaAYZC",
+        "output_dir": "mods",
+    },
+    {
+        "name": "tweakeroo",
+        "source": Source.CURSEFORGE,
+        "mod_id": 297344,
+        "output_dir": "mods",
+    },
+    {
+        "name": "litematica",
+        "source": Source.CURSEFORGE,
+        "mod_id": 308892,
+        "output_dir": "mods",
+    },
+#    {
+#        "name": "mini_hud",
+#        "source": Source.CURSEFORGE,
+#        "mod_id": 244260,
+#        "output_dir": "mods",
+#    },
+]
+
 def get_input_num():
-    choice = input(" > ")
-    try:
-        return int(choice)
-    except ValueError:
-        return None
+    while True:
+        choice = input(" > ")
+        try:
+            ret = int(choice)
+            break
+        except ValueError:
+            print("Try again")
+    return ret
 
 def mod_exists(dir:str, name:str):
     return pathlib.Path(dir).glob(f"{name}--*")
@@ -56,9 +200,11 @@ def update_curse(game_version: str, entry: dict):
 
     r = requests.get(f"{CURSE_BASE_URL}/v1/mods/{entry['mod_id']}/files", headers=curse_headers, params=params)
 
+    default_curse_file = None
+
     data = r.json()["data"]
     data.sort(reverse=True, key=lambda item : item["fileDate"])
-    print("\nChoose an option (CURSEFORGE):")
+    print(f"\nChoose an option for {entry['name']} (CURSEFORGE):")
     for i, curse_file in enumerate(data[0:min(MAX_ENTRIES, len(data))]):
         is_curr_string = ""
         curr_file = pathlib.Path(entry["output_dir"], f"[{entry['name']}]{curse_file['fileName']}")
@@ -68,14 +214,18 @@ def update_curse(game_version: str, entry: dict):
             default_curse_file = curse_file
         print(f"\t{i}: {curse_file['displayName']}  ({curse_file['downloadCount']} downloads){is_curr_string}")
 
-    # get choice
-    choice = get_input_num()
-    
-    # save off needed data
-    if choice is None:
+    if default_curse_file is data[0]:
         curse_file = default_curse_file
     else:
-        curse_file = data[choice]
+        # get choice
+        choice = get_input_num()
+        
+        # save off needed data
+        if choice is None:
+            curse_file = default_curse_file
+        else:
+            curse_file = data[choice]
+    
     #pprint(curse_file)
     entry['file_name'] = curse_file['fileName']
     entry['download_url'] = curse_file['downloadUrl']
@@ -84,16 +234,24 @@ def update_curse(game_version: str, entry: dict):
 
 def update_modrinth(game_version: str, entry: dict):
     r = requests.get(f"{MODRINTH_BASE_URL}api/v1/mod/{entry['mod_id']}/version")
-    pprint(r.json())
+    #pprint(r.json())
 
     def get_filename(modrinth_file):
         return modrinth_file['files'][0]["filename"]
 
     data = r.json()
     data.sort(reverse=True, key=lambda item : item["name"])
-    print("\nChoose an option (MODRINTH):")
+
+    default_modrinth_file = None
+
+    print(f"\nChoose an option for {entry['name']} (MODRINTH):")
     for i, modrinth_file in enumerate(data[0:min(MAX_ENTRIES, len(data))]):
         is_curr_string = ""
+
+        # hotifx
+        if len(modrinth_file['files']) <= 0:
+            continue
+
         curr_file = pathlib.Path(entry["output_dir"], f"[{entry['name']}]{get_filename(modrinth_file)}")
         if curr_file.exists():
             is_curr_string = "  CURRENT"
@@ -101,14 +259,18 @@ def update_modrinth(game_version: str, entry: dict):
             default_modrinth_file = modrinth_file
         print(f"\t{i}: {modrinth_file['name']}  ({modrinth_file['downloads']} downloads){is_curr_string}")
 
-    # get choice
-    choice = get_input_num()
-    
-    # save off needed data
-    if choice is None:
+    if default_modrinth_file is data[0]:
         modrinth_file = default_modrinth_file
     else:
-        modrinth_file = data[choice]
+        # get choice
+        choice = get_input_num()
+        
+        # save off needed data
+        if choice is None:
+            modrinth_file = default_modrinth_file
+        else:
+            modrinth_file = data[choice]
+    
     #pprint(curse_file)
     entry['file_name'] = get_filename(modrinth_file)
     entry['download_url'] =  modrinth_file['files'][0]["url"]
@@ -118,42 +280,10 @@ def update_modrinth(game_version: str, entry: dict):
 def generate_modlist(modlist_file: str):
     game_version = "1.18"
     # name is just for ease of reading 
-    source_template = [
-        {
-            "name": "fabric_api",
-            "source": Source.CURSEFORGE,
-            "mod_id": 306612,
-            "output_dir": "mods",
-        },
-        {
-            "name": "ding",
-            "source": Source.CURSEFORGE,
-            "mod_id": 231275,
-            "output_dir": "mods",
-        },
-        {
-            "name": "bears_armour_hud",
-            "source": Source.CURSEFORGE,
-            "mod_id": 420155,
-            "output_dir": "mods",
-        },
-        {
-            "name": "better_taskbar",
-            "source": Source.MODRINTH,
-            "mod_id": "gPEcet33",
-            "output_dir": "mods",
-        },
-        {
-            "name": "cloth_config",
-            "source": Source.MODRINTH,
-            "mod_id": 319057,
-            "output_dir": "mods",
-        }
-    ]
 
     output = list()
 
-    for entry in source_template:
+    for entry in SOURCE_TEMPLATE:
         result = None
         if entry["source"] == Source.CURSEFORGE:
             result = update_curse(game_version, entry)
